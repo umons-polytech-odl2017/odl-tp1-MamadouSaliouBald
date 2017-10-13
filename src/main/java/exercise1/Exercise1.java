651afef569ec63e2d2945f7c897bb1eddfe7ac8e
131542 <MamadouSalioubald>
2017-10-13 16:43:33 +0200

    hollo word

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,26 @@
 package exercise1;
 
+import javax.swing.*;
+
 public class Exercise1 {
-	static Person createPerson(String name, int age) {
+	static Person createPerson(String name, int age)
+	{
 		// Ajoutez les champs name et age à la classe Person.
+		Person p1 = new Person("Jean",20);
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		return new Person(name, age);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+		String name = args[0];
+		int age = Integer.parseInt(args[1]);
+
+		Person p = new Person(name, age);
+		System.out.println("Persone :" + p.toString() +" , "+p.getAge());
 	}
 }
